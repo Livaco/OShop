a3b06dfdd3b0eba3b940bfc5c9c701b7429a78b5
Wizered <64568264+Wizered@users.noreply.github.com>
2020-05-25 13:48:23 -0600

    Create gardeningsupplies.lua
    Creates the category and entities needed for GrowOP

---
+++ omega_shop/lua/oshop/items/gardeningsupplies.lua
@@ -0,0 +1,133 @@
+// GrowOP category.
+OShop.CreateCategory("Gardening Supplies", {
+  description = "Good for... ehem, 'gardening'.",
+  color = Color(15, 150, 15, 255)
+})
+
+OShop.CreateItem("Flowerpot", {
+  category = "Gardening Supplies",
+  itemtype = "entity",
+  class = "zwf_pot",
+  description = "A pot... for pot.",
+  price = 20
+})
+
+OShop.CreateItem("Hydro Flowerpot", {
+  category = "Gardening Supplies",
+  itemtype = "entity",
+  class = "zwf_pot_hydro",
+  description = "A pot that waters itself! Truly we are living in the future.",
+  price = 250
+})
+
+OShop.CreateItem("Water Tank", {
+  category = "Gardening Supplies",
+  itemtype = "entity",
+  class = "zwf_watertank",
+  description = "The giver of life...and water.",
+  price = 75
+})
+
+OShop.CreateItem("Soil", {
+  category = "Gardening Supplies",
+  itemtype = "entity",
+  class = "zwf_soil",
+  description = "Fancy dirt.",
+  price = 10
+})
+
+OShop.CreateItem("Generator", {
+  category = "Gardening Supplies",
+  itemtype = "entity",
+  class = "zwf_generator",
+  description = "Get the power, generator power.",
+  price = 300
+})
+
+OShop.CreateItem("Fuel Jug", {
+  category = "Gardening Supplies",
+  itemtype = "entity",
+  class = "zwf_fuel",
+  description = "Real Fuel! Not like that fake fuel.",
+  price = 20
+})
+
+OShop.CreateItem("Power Strip", {
+  category = "Gardening Supplies",
+  itemtype = "entity",
+  class = "zwf_outlet",
+  description = "Guides the power.",
+  price = 10
+})
+
+OShop.CreateItem("Fan", {
+  category = "Gardening Supplies",
+  itemtype = "entity",
+  class = "zwf_ventilator",
+  description = "Blow the air everywhere.",
+  price = 15
+})
+
+OShop.CreateItem("Lamp", {
+  category = "Gardening Supplies",
+  itemtype = "entity",
+  class = "zwf_lamp",
+  description = "Let there be light!.",
+  price = 75
+})
+
+OShop.CreateItem("Table", {
+  category = "Gardening Supplies",
+  itemtype = "entity",
+  class = "zwf_packingstation",
+  description = "Better than packing on the floor.",
+  price = 25
+})
+
+OShop.CreateItem("Mixer", {
+  category = "Gardening Supplies",
+  itemtype = "entity",
+  class = "zwf_mixer",
+  description = "Great for mixing ingredients.",
+  price = 30
+})
+
+OShop.CreateItem("Oven", {
+  category = "Gardening Supplies",
+  itemtype = "entity",
+  class = "zwf_oven",
+  description = "Necessary to make baked goods.",
+  price = 20
+})
+
+OShop.CreateItem("Palette", {
+  category = "Gardening Supplies",
+  itemtype = "entity",
+  class = "zwf_palette",
+  description = "Can carry quite a lot of finished product.",
+  price = 10
+})
+
+OShop.CreateItem("Drying Station", {
+  category = "Gardening Supplies",
+  itemtype = "entity",
+  class = "zwf_drystation",
+  description = "Nothing is more depressing than a soggy plant.",
+  price = 15
+})
+
+OShop.CreateItem("Cable", {
+  category = "Gardening Supplies",
+  itemtype = "weapons",
+  class = "zwf_cable",
+  description = "Needed to power up your setup.",
+  price = 15
+})
+
+OShop.CreateItem("Watering Can", {
+  category = "Gardening Supplies",
+  itemtype = "weapons",
+  class = "zwf_wateringcan",
+  description = "Needed to power up your setup.",
+  price = 5
+})
