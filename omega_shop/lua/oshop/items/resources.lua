a7fac8121198b30c4d9bc2d4cae7f52ec0acb6f9
Wizered <64568264+Wizered@users.noreply.github.com>
2020-05-26 15:14:13 -0600

    Add resources.lua
    Adds the bCrafting resources to OShop

---
+++ omega_shop/lua/oshop/items/resources.lua
@@ -0,0 +1,87 @@
+-- bCrafting Resources category.
+
+OShop.CreateCategory("Resources", {
+  description = "Useful for making guns!",
+  color = Color(0, 200, 0, 255)
+})
+
+OShop.CreateItem("Gunpowder", {
+  category = "Resources",
+  itemtype = "entity",
+  class = "bcrafting_resource_gunpowder",
+  setowning_ent = false,
+  description = "Highly flammable!",
+  price = 25
+})
+
+OShop.CreateItem("Rope", {
+  category = "Resources",
+  itemtype = "entity",
+  class = "bcrafting_resource_rope",
+  setowning_ent = false,
+  description = "Look Gordon, ropes!",
+  price = 50
+})
+
+OShop.CreateItem("Elastic Material", {
+  category = "Resources",
+  itemtype = "entity",
+  class = "bcrafting_resource_elasticmaterial",
+  setowning_ent = false,
+  description = "Stretchy!",
+  price = 75
+})
+
+OShop.CreateItem("Wood", {
+  category = "Resources",
+  itemtype = "entity",
+  class = "bcrafting_resource_wood",
+  setowning_ent = false,
+  description = "Just like my personality!",
+  price = 100
+})
+
+OShop.CreateItem("Plastic", {
+  category = "Resources",
+  itemtype = "entity",
+  class = "bcrafting_resource_plastic",
+  setowning_ent = false,
+  description = "One Word: Plastics.",
+  price = 125
+})
+
+OShop.CreateItem("Metal", {
+  category = "Resources",
+  itemtype = "entity",
+  class = "bcrafting_resource_metal",
+  setowning_ent = false,
+  description = "That's pretty hardcore.",
+  price = 150
+})
+
+OShop.CreateItem("Glass", {
+  category = "Resources",
+  itemtype = "entity",
+  class = "bcrafting_resource_glass",
+  setowning_ent = false,
+  description = "Don't drop it!",
+  price = 175
+})
+
+OShop.CreateItem("Machinery", {
+  category = "Resources",
+  itemtype = "entity",
+  class = "bcrafting_resource_machinery",
+  setowning_ent = false,
+  description = "Looks complicated.",
+  price = 200
+})
+
+OShop.CreateItem("Explosive Material", {
+  category = "Resources",
+  itemtype = "entity",
+  class = "bcrafting_resource_explosivematerial",
+  setowning_ent = false,
+  description = "Looks pretty dangerous",
+  price = 250
+})
